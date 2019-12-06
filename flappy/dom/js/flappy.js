bdf5dac6170e4f9a55db582ea632b2789f78c558
gealcan <gealcan.neto@gmail.com>
2019-12-06 02:05:03 -0300

    dando push no flappy

---
+++ flappy/dom/js/flappy.js
@@ -0,0 +1,184 @@
+
+// criar novos elementos
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName)
+    elem.className = className
+    return elem
+}
+// criar barreiras
+function Barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')  // 
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)  
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`  // mudar altura do corpo
+}
+
+ 
+//const b = new Barreira(true)
+//b.setAltura(300)
+//document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)  // sorteia aleatoriamente a altura superior, ja que abertura é fixa e vem como parametro
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+//const b = new ParDeBarreiras(700, 400, 1000)
+//document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
+    const deslocamento = 3 // velocidade que andam os pares de barreiras
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento)
+
+            // quando o elemento sair da area do jogo  x = left = 0
+            if (par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length)  // vai pro final
+                par.sortearAbertura() // para nao ficar repetitivo
+            }
+
+            const meio = largura / 2
+            const cruzouOMeio = par.getX() + deslocamento >= meio
+                && par.getX() < meio
+                if(cruzouOMeio) 
+                    notificarPonto()
+        })
+    }
+}
+
+function Codorna(alturaJogo) {
+    let voando = false
+
+    this.elemento = novoElemento('img', 'codorna')
+    this.elemento.src = 'imgs/codorna.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+    
+    window.onkeydown = e => voando = true // qualquer tecla que aperta
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5 ) // ajuste de voo
+        const alturaMax = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0) {
+            this.setY(0)
+        } else if (novoY >= alturaMax) {
+            this.setY(alturaMax)
+        } else {
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
+
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
+/*
+const barreiras = new Barreiras(700, 1200, 400, 400)
+const codorna = new Codorna(700)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+
+areaDoJogo.appendChild(codorna.elemento)
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+setInterval(() => {
+    barreiras.animar()
+    codorna.animar()
+}, 20)
+
+*/
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left
+            && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top
+            && b.top + b.height >= a.top 
+    return horizontal && vertical
+
+}
+
+function colidiu(codorna, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(ParDeBarreiras => {
+        if (!colidiu) {
+            const superior = ParDeBarreiras.superior.elemento
+            const inferior = ParDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(codorna.elemento, superior)
+                || estaoSobrepostos(codorna.elemento, inferior)
+        }
+    })
+
+    return colidiu
+}
+
+function FlappyQuail() {
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 400,
+        () => progresso.atualizarPontos(++pontos))
+    const codorna = new Codorna(altura)
+
+    areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(codorna.elemento)
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+    
+    this.start = () => {
+        // loop do jogo
+        const temporizador = setInterval(() => {
+            barreiras.animar()
+            codorna.animar()
+
+            if(colidiu(codorna, barreiras)) {
+                clearInterval(temporizador)
+            }
+        }, 20)
+    }
+}
+
+new FlappyQuail().start()
